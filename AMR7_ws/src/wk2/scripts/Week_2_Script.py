0c1ae2f44209bf8c1d8926a526eb87352a161cd5
Onis.brown <Onis.brown@hotmail.co.uk>
2018-02-12 12:47:47 +0000

    week 2 done week 3 start

--- AMR7_ws/src/wk2/scripts/Week_2_Script.py
+++ AMR7_ws/src/wk2/scripts/Week_2_Script.py
@@ -6,4 +6,17 @@ import roslib
 from std_msgs.msg import String
 from geometry_msgs.msg import Twist
 
-move = twist()
+rospy.init_node('robot_mover', anonymous=True)
+velocity_publisher = rospy.Publisher('/cmd_vel', Twist, queue_size=100)
+move = Twist()
+
+move.linear.y = 0.0
+move.linear.x = 0.1
+move.angular.x = 0.0
+move.angular.y = 1.0
+move.angular.z = 0.4
+
+
+
+while not rospy.is_shutdown():
+    velocity_publisher.publish(move)
